5991326455db19396c8d90c0426c8741860613ba
Pim van Leeuwen <pimvleeuwen@outlook.com>
2024-10-12 16:08:35 +0200

    Added comments

--- grammars/java/java_parser.py
+++ grammars/java/java_parser.py
@@ -4,6 +4,36 @@ from grammars.java.JavaParser import JavaParser
 from grammars.java.JavaParserListener import JavaParserListener
 from tree.abstract_tree import *
 
+# Create a listener for the Java project, from this we create the nodes and content of the nodes into the target
+# format. Overrides the methods from JavaParserListener
+class MyJavaListener(JavaParserListener):
+
+    # Import declarations
+    def enterImportDeclaration(self, ctx: JavaParser.ImportDeclarationContext):
+        print("Import:", ctx.qualifiedName().getText())
+
+    # Enter class creations
+    def enterClassDeclaration(self, ctx:JavaParser.classDeclaration):
+        print("Enter ClassBody:", ctx.identifier().getText())
+
+    # Exits classes
+    def exitClassDeclaration(self, ctx:JavaParser.classDeclaration):
+        print("Exit ClassBody:", ctx.identifier().getText())
+
+    # Call another method
+    def enterMethodCall(self, ctx: JavaParser.MethodCallContext):
+        print("Enter Method Call:", ctx.identifier().getText())
+        print("Method Params:", ctx.arguments().getText())
+
+    # Enter method creation
+    def enterMethodDeclaration(self, ctx: JavaParser.MethodDeclarationContext):
+        print("Enter Method Declaration:", ctx.identifier().getText())
+        print("Method content:", ctx.getText())
+
+    # Exit method
+    def exitMethodDeclaration(self, ctx: JavaParser.MethodDeclarationContext):
+        print("Exit Method Declaration:", ctx.identifier().getText())
+
 def parse_java_file(path, file):
     input_stream = FileStream(path, encoding='utf-8')
     lexer = JavaLexer(input_stream)
@@ -11,43 +41,19 @@ def parse_java_file(path, file):
     parser = JavaParser(stream)
     tree = parser.compilationUnit()
 
+    # This will be the full node of the file
     return_node = ASTNode(file, ASTNodeType.FILE)
+    # This will be the node that we are currently add throughout the process. Since we only go up or down one level
+    # at a time this should work out
+    # Still need to work out how to do this through the listener
     current_node = return_node
 
-    # Create a listener for the Java project
-    class MyJavaListener(JavaParserListener):
-
-        def enterImportDeclaration(self, ctx: JavaParser.ImportDeclarationContext):
-            print("Import:", ctx.qualifiedName().getText())
-
-        def enterClassDeclaration(self, ctx:JavaParser.classDeclaration):
-            print("Enter ClassBody:", ctx.identifier().getText())
-
-        def exitClassDeclaration(self, ctx:JavaParser.classDeclaration):
-            print("Exit ClassBody:", ctx.identifier().getText())
-
-        def enterMethodCall(self, ctx: JavaParser.MethodCallContext):
-            print("Enter Method Call:", ctx.identifier().getText())
-            print("Method Params:", ctx.arguments().getText())
-
-        def exitMethodCall(self, ctx: JavaParser.MethodCallContext):
-            print("Exit Method Call:", ctx.identifier().getText())
-
-        def enterMethodDeclaration(self, ctx: JavaParser.MethodDeclarationContext):
-            print("Enter Method Declaration:", ctx.identifier().getText())
-            print("Method content:", ctx.getText())
-
-        def exitMethodDeclaration(self, ctx: JavaParser.MethodDeclarationContext):
-            print("Exit Method Declaration:", ctx.identifier().getText())
-
-
-        #
-        # def enterMethodCall(self, ctx: JavaParser.MethodCallContext):
-        #     print("Method Call:", ctx.getText())
-        #
-        # def enterMethodDeclaration(self, ctx:JavaParser.MethodDeclarationContext):
-        #     print("Method Declaration:", ctx.getText())
-
+    # This is a default Tree Walker
     walker = ParseTreeWalker()
+    # This is the adapter JavaParserListener that we created above to adapt to our tree
     listener = MyJavaListener()
+    # Perform the walk
     walker.walk(listener, tree)
+
+    # return the node of this file and then we parsed a Java file
+    return return_node
